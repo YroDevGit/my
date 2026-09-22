ad360220a63f07b73c3f719c3f5928966cf4208a
Tyrone <tyrone@clearmindai.com>
2026-09-22 08:29:18 +0800

    new

--- views/js/user/login.js
+++ views/js/user/login.js
@@ -7,15 +7,6 @@ import ImageSelector from "../../code/src/mods/picker/imageselector.js";
 import Twal from "../../code/src/mods/twal.js";
 import Tyrax from "../../code/src/tyrux/main.js";
 
-
-let mod = Popmodal.init("#loginForm",{
-
-});
-
-setTimeout(() => {
-    mod.open;
-}, 2000);
-
 Ctr.submit("#loginForm", (data, raw) => {
     Tyrax.post({
         url: "login",
